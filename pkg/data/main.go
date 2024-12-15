433946ef458d044a50b7bde175b58f55e69dd9b5
Alexander Utterodt <alexander.utterodt@gmail.com>
2024-12-15 19:28:11 +0100

    Added parameter_handler,Locked test (failing)

--- pkg/data/main.go
+++ pkg/data/main.go
@@ -1,1 +1,12 @@
 package data
+
+import "math"
+
+const (
+	DefaultParameterName    string  = "Parameter"
+	DefaultParameterValue   float64 = 10.0
+	DefaultParameterDefault float64 = 0.0
+	DefaultParameterMin     float64 = -math.MaxFloat64
+	DefaultParameterMax     float64 = math.MaxFloat64
+	DefaultParameterCheck   bool    = false
+)
